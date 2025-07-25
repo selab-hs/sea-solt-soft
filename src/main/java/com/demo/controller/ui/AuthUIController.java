d4a2c489b70c14bb336bf618ddf7876c9005d2b4
Duskafka <rlarlgus0206@naver.com>
2025-07-25 17:09:04 +0900

    feat(View): 로그인 뷰 컨트롤러
    - `/sign-in` URL 접근하면 로그인 페이지가 띄워지도록 함.

---
+++ src/main/java/com/demo/controller/ui/AuthUIController.java
@@ -0,0 +1,13 @@
+package com.demo.controller.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthUIController {
+
+    @RequestMapping("/sign-in")
+    public String signIn() {
+        return "/login/login-1.0";
+    }
+}
