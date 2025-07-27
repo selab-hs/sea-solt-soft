479c3e7dd6ffd9d95f2e217522369abb3dcdaf4e
PDJ-N <qkreo321@gmail.com>
2025-07-27 21:31:04 +0900

    feat(ui-controller): 회원가입(sign-up) 페이지 라우팅 추가
    - AuthUIController에 /sign-up 요청을 처리하는 메서드 추가
    - "/signup/signup-1.0" 템플릿 반환
    - 기존 로그인 화면 라우팅과 동일한 방식으로 구성

--- src/main/java/com/demo/controller/ui/AuthUIController.java
+++ src/main/java/com/demo/controller/ui/AuthUIController.java
@@ -8,6 +8,12 @@ public class AuthUIController {
 
     @RequestMapping("/sign-in")
     public String signIn() {
+
         return "/login/login-1.0";
+    }
+
+    @RequestMapping("/sign-up")
+    public String signUp() {
+        return "/signup/signup-1.0";
     }
 }
